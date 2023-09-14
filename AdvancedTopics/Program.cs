011cf93b300865be5ea7910803421235d0fd9e7a
AzizbekSharifovich <azizbek0698@gmail.com>
2023-09-14 13:40:44 +0500

    Learning Threads Through The Breakfast Project

--- AdvancedTopics/Program.cs
+++ AdvancedTopics/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿ using System;
+using System.Threading;
 
 namespace AdvancedTopics;
 
@@ -6,6 +7,27 @@ public class Program
 {
     static void Main(string[] args)
     {
+        Thread teaMakingProccess = new Thread(MakeTea);
+        teaMakingProccess.IsBackground = true;
+        teaMakingProccess.Start();
 
+        FryEggs();
+
+        Console.WriteLine("Bon appetit!");
+    }
+
+    public static void MakeTea()
+    {
+        Console.WriteLine("Bioling water!");
+        Thread.Sleep(5000);
+        Console.WriteLine("Water is boiled. Pouring into cup... .");
+        Console.WriteLine("Tea is ready!");
+    }
+
+    public static void FryEggs()
+    {
+        Console.WriteLine("Frying eggs... .");
+        Thread.Sleep(1000);
+        Console.WriteLine("Eggs are ready!");
     }
 }
