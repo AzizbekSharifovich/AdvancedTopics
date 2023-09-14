e302fd8936c7ee59a36ecc63d4e3feda1e85a868
AzizbekSharifovich <azizbek0698@gmail.com>
2023-09-14 17:35:58 +0500

    Learning Basic Task

--- AdvancedTopics/Program.cs
+++ AdvancedTopics/Program.cs
@@ -1,5 +1,6 @@
-﻿ using System;
+﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace AdvancedTopics;
 
@@ -7,25 +8,37 @@ public class Program
 {
     static void Main(string[] args)
     {
-        ThreadPool.QueueUserWorkItem(MakeTea, 4);
+        Task<string> boildWaterTask = Task.Run(() => BoilWater());
+
+        Task makeTeaTask = boildWaterTask.ContinueWith(
+            (water) => MakeTea(water.Result));
 
         FryEggs();
 
+        makeTeaTask.Wait();
         Console.WriteLine("Bon appetit!");
     }
 
-    public static void MakeTea(object state)
+    public static void MakeTea( string water)
+    {
+        Console.WriteLine("Putting teabag into cup... ");
+        Thread.Sleep(1000);
+        Console.WriteLine($"Pouring {water}");
+        Console.WriteLine("Tea is ready!");
+    }
+
+    public static string BoilWater()
     {
         Console.WriteLine("Bioling water!");
         Thread.Sleep(5000);
-        Console.WriteLine("Water is boiled. Pouring into cup... .");
-        Console.WriteLine("Tea is ready!");
+        Console.WriteLine("Water is boiled!");
+        return "Water!";
     }
 
     public static void FryEggs()
     {
         Console.WriteLine("Frying eggs... .");
-        Thread.Sleep(1000);
+        Thread.Sleep(2000);
         Console.WriteLine("Eggs are ready!");
     }
 }
